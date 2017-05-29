f5ae1f61fd0c092bac0cb5dbe3be974c2ce95ff7
LizcGitHub <304829558@qq.com>
2017-05-29 15:39:17 +0800

    刷~

--- app/src/main/java/com/study/zouchao/finalexamproject_two/downloaddata/downloaded/model/dto/DownloadedSuccessUpdateUiDTO.java
+++
@@ -1,9 +0,0 @@
-package com.study.zouchao.finalexamproject_two.downloaddata.downloaded.model.dto;
-
-/**
- * Created by Administrator on 2017/2/14.
- */
-
-public class DownloadedSuccessUpdateUiDTO {
-
-}
